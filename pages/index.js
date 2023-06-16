330f10aae2ff2169b004e01358bdc3f05f815bb7
Charlie Holtz <charlieholtz@gmail.com>
2023-06-16 11:48:23 -0400

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -148,6 +148,10 @@ export default function Home() {
         <meta property="og:image" content={ogImage} />
         <meta property="twitter:image" content={ogImage} />
         <meta name="twitter:card" content="summary_large_image" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔳</text></svg>"
+        />
       </Head>
 
       <div className="text-center">
