40a81d8ff42076781aa5f8ae9b0b9d041db5fc39
Charlie Holtz <charlieholtz@gmail.com>
2023-06-16 00:44:44 -0400

    promptmaker

--- pages/index.js
+++ pages/index.js
@@ -198,6 +198,7 @@ export default function Home() {
         <div class="mt-6">
           <button
             type="button"
+            onClick={() => setPrompt(promptmaker())}
             className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           >
             <svg
